1d8afe88c0650909d4e33765694f09ae711d1b5f
Molly Heston <mheston@iastate.edu>
2024-10-05 23:35:25 -0500

    test

---
+++ 06_3_20_Activity11_In_class_Challenges_MoviesFromJSON.js
@@ -0,0 +1,13 @@
+
+
+function showCardsSortedByPriceLowHigh() {
+    fetch("./MoviesFromJSON.json")
+    .then(response => response.json())
+    .then(myMovies => loadMovies(myMovies, 1))
+    .catch(err => console.log("Error :"+err));
+}
+/*
+    Author: Molly Heston
+    ISU Netid : mheston@iastate.edu1
+    Date : 09 27, 2024
+*/
